4234e6222b094ebe3e5e31ba953c54f50733893c
Brody <brodyf327@gmail.com>
2020-07-22 21:33:32 -0400

    Added updating users table functionality and started adding create post/update post functionality.

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,19 @@
-const { client, getAllUsers, createUser } = require('./index');
+const { client, getAllUsers, createUser, updateUser } = require('./index');
 
 async function testDB() {
     try {
         console.log("Starting to test database...");
 
+        console.log("Calling getAllUsers")
         const users = await getAllUsers();
-        console.log("getAllUsers:", users);
+        console.log("Result:", users);
+
+        console.log("Calling updateUser on users[0]")
+        const updateUserResult = await updateUser(users[0].id, {
+            name: "Newname Sogood",
+            location: "Lesterville, KY"
+        });
+        console.log("Result:", updateUserResult);
 
         console.log("Finished testing database!");
     } catch (error) {
@@ -19,6 +27,7 @@ async function dropTables() {
         console.log("Starting to drop tables...");
 
         await client.query(`
+        DROP TABLE IF EXISTS posts;
         DROP TABLE IF EXISTS users;
         `);
 
@@ -37,7 +46,20 @@ async function createTables() {
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
-            password varchar(255) NOT NULL
+            password varchar(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255),
+            active BOOLEAN DEFAULT true
+        );
+        `);
+
+        await client.query(`
+        CREATE TABLE posts (
+            id SERIAL PRIMARY KEY,
+            "authorId" INTEGER REFERENCES users(id) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            content TEXT NOT NULL,
+            active BOOLEAN DEFAULT true
         );
         `);
 
@@ -52,9 +74,9 @@ async function createInitialUsers() {
     try {
         console.log("Starting to create users...");
 
-        const albert = await createUser({ username: "albert", password: "bertie99" });
-        const sandra = await createUser({ username: "sandra", password: "2sandy4me" });
-        const glamgal = await createUser({ username: "glamgal", password: "soglam" });
+        const albert = await createUser({ username: "albert", password: "bertie99", name: "Albert", location: "New York" });
+        const sandra = await createUser({ username: "sandra", password: "2sandy4me", name: "Sandra", location: "Charleston" });
+        const glamgal = await createUser({ username: "glamgal", password: "soglam", name: "Barbie", location: "Los Angeles" });
 
         console.log(albert);
 
